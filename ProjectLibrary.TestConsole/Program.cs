1bf77698b1c576fc215f191698f0b7960423ee64
Samuel Legrain <samuel.legrain@bstorm.be>
2026-01-29 14:49:20 +0100

    Add TestConsole Project

---
+++ ProjectLibrary.TestConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿using ProjectLibrary.DAL.Entities;
+using ProjectLibrary.DAL.Services;
+
+namespace ProjectLibrary.TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookService service = new BookService();
+
+            IEnumerable<Book> library = service.Get();
+
+            foreach (Book book in library)
+            {
+                Console.WriteLine($"\"{book.Title}\" écrit par {book.Author}");
+            }
+        }
+    }
+}
